daad1821fa20ffcc28cd02542e1fae9ac4c5062e
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-01 16:12:22 +0100

    HomeCompo (lazyload)

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from "./home.component";
 
-
-
+console.warn("HOME LOADED LAZLY")
 @NgModule({
   declarations: [
+
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
