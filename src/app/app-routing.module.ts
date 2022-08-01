a5d46bb32a529d161677d61b227ff74b4190f4b7
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-01 14:15:22 +0100

    add canActivate fb guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {FacebookGuard} from "./guards/facebook.guard";
 
 const routes: Routes = [
   {
     path:'',
-    loadChildren: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    loadChildren: () => import('./components/home/home.component').then(m => m.HomeComponent),
+    //Angural access not anyone can get into Home
+    canActivate:[FacebookGuard]
 
   },
 
