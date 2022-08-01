e12423e570bdc73d513e79bda6dc19d01bbc447f
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-01 14:08:47 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./components/home/home.component').then(m => m.HomeComponent)
+
+  },
+
+  {
+    path:'login',
+    component:LoginComponent
+  },
+
+  {
+    path:'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
