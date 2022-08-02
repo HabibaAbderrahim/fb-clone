092016607dcfec03a5a54fc3d99fca55aa06262b
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-02 16:09:55 +0100

    add needed modules

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,16 +3,34 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from "./home.component";
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {FormsModule} from "@angular/forms";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardAvatar, MatCardModule} from "@angular/material/card";
 
 console.warn("HOME LOADED LAZLY")
 @NgModule({
   declarations: [
 
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule,
+    MatBadgeModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule
+
+
+
   ]
 })
 export class HomeModule { }
