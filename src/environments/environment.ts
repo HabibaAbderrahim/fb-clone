1dd106ce8796b9388f089a00759f15792a89cbf0
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-03 12:07:45 +0100

    config fire base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+  firebaseConfig :{
+    apiKey: "AIzaSyCT1le8x6YvZIggFqiyFfBRbbG378k7jV0",
+    authDomain: "fb-clone-77e35.firebaseapp.com",
+    projectId: "fb-clone-77e35",
+    storageBucket: "fb-clone-77e35.appspot.com",
+    messagingSenderId: "875839240594",
+    appId: "1:875839240594:web:20c442dca1beb12c01a94b"
+  },
 };
 
 /*
