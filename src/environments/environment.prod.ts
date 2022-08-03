89c7365103800a72c05684761d99dd6449c64d4f
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-03 12:08:44 +0100

    commit repo build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Your web app's Firebase configuration
+  firebaseConfig : {
+    apiKey: "AIzaSyCT1le8x6YvZIggFqiyFfBRbbG378k7jV0",
+    authDomain: "fb-clone-77e35.firebaseapp.com",
+    projectId: "fb-clone-77e35",
+    storageBucket: "fb-clone-77e35.appspot.com",
+    messagingSenderId: "875839240594",
+    appId: "1:875839240594:web:20c442dca1beb12c01a94b"
+  },
 };
