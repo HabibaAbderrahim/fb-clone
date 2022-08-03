7ea78f765246955b1887c7f3cd660107149fa398
HabibaAbderrahim <heububabiba@gmail.com>
2022-08-03 19:10:22 +0100

    add matcardModule

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -6,10 +6,14 @@ import {HomeComponent} from "./home.component";
 import {NavBarComponent} from "../nav-bar/nav-bar.component";
 import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, NgForm} from "@angular/forms";
 import {MatBadgeModule} from "@angular/material/badge";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardAvatar, MatCardModule} from "@angular/material/card";
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../../../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 console.warn("HOME LOADED LAZLY")
 @NgModule({
@@ -26,8 +30,11 @@ console.warn("HOME LOADED LAZLY")
     MatBadgeModule,
     FormsModule,
     MatButtonModule,
-    MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    MatCardModule
 
 
 
